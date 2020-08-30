d69f34cf8d6dc2ba0d2aa596ff774b3286142467
maryam aalizadeh <maalizadeh7394@gmail.com>
2020-08-31 00:20:38 +0430

    delete extra elements

--- Pattern.go
+++ Pattern.go
@@ -3,15 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World");
 
-	//var i, j int
 	var number int
 	fmt.Println("Please Enter your number: ")
 	fmt.Scan(&number)
 
-	if number == 1 {
-		println("Nothing")
+	if number < 1 {
+		println("*")
 	}else {
 		for i := 1 ; i <= number ; i++ {
 			for j := number ; j >= i ; j-- {
