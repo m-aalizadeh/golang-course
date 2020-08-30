d9d5ad68fe9c7719064feff7cfdceaa99ab0be61
maryam aalizadeh <maalizadeh7394@gmail.com>
2020-08-31 00:27:28 +0430

    check negative number

--- Sum.go
+++ Sum.go
@@ -5,12 +5,12 @@ import "fmt"
 func main() {
 
 	var number int
-	var sum int = 0
+	var sum = 0
 	fmt.Println("Please Enter your number: ")
 	fmt.Scan(&number)
 
-	if number == 1 {
-		fmt.Println("One")
+	if number < 1 {
+		fmt.Println("The number must be positive.")
 	}else {
 		for i := 1 ; i <= number ; i++ {
 			sum += i
